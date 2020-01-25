8aeb84d767ad38d0cfb9a82f213e362f36045d5a
Laura Collins <laura.e.collins1@gmail.com>
2020-01-24 19:28:42 -0600

    form works

--- src/components/pages/SightingForm/SightingForm.js
+++ src/components/pages/SightingForm/SightingForm.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 import './SightingForm.scss';
-// import authData from '../../../helpers/data/authData';
+import authData from '../../../helpers/data/authData';
 import snakelingsData from '../../../helpers/data/snakelingsData';
 import statesData from '../../../helpers/data/statesData';
+import sightingsData from '../../../helpers/data/sightingsData';
 
 class SightingForm extends React.Component {
   state = {
@@ -16,6 +17,23 @@ class SightingForm extends React.Component {
     description: '',
     snakes: [],
     states: [],
+  }
+
+  saveSightingEvent = (e) => {
+    e.preventDefault();
+    const newSighting = {
+      identified: this.state.identified,
+      snakeId: this.state.snakeId,
+      stateId: this.state.stateId,
+      county: this.state.county,
+      imageUrl: this.state.imageUrl,
+      uid: authData.getUid(),
+      dateFound: this.state.dateFound,
+      description: this.state.description,
+    };
+    sightingsData.saveSighting(newSighting)
+      .then(() => this.props.history.push('/sightings'))
+      .catch((error) => console.error('err from save sighting', error));
   }
 
   getStates = () => {
@@ -69,66 +87,132 @@ class SightingForm extends React.Component {
     this.setState({ county: e.target.value });
   }
 
+  urlChange = (e) => {
+    e.preventDefault();
+    this.setState({ imageUrl: e.target.value });
+  }
+
+  changeIdentified = (e) => {
+    const checkedValue = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+    this.setState({ identified: checkedValue });
+  }
+
+  descriptionChange = (e) => {
+    e.preventDefault();
+    this.setState({ description: e.target.value });
+  }
+
   render() {
     const {
       dateFound,
       county,
-      // identified,
-      // snakeId,
-      // imageUrl,
-      // description,
+      identified,
+      imageUrl,
+      description,
       snakes,
       states,
     } = this.state;
 
     return (
       <div className="SightingForm">
-        <form>
-        <div className="form-group">
-        <label htmlFor="date-found">Date Found</label>
-          <input
-            type="date"
-            className="form-control col-2 m-auto"
-            id="date-found"
-            value={dateFound}
-            onChange={this.dateChange}
+        <form className="formContainer">
+          <div className="form-inline d-flex justify-content-center">
+            <div className="form-group row justify-content-center">
+              <label htmlFor="date-found col-form-label">Date Found</label>
+              <input
+                type="date"
+                className="form-control m-2"
+                id="date-found"
+                value={dateFound}
+                onChange={this.dateChange}
+                >
+              </input>
+           </div>
+          </div>
+        <div className="form-inline d-flex justify-content-center">
+          <div className="form-group row">
+            <label htmlFor="state-name" className="col-form-label">Select State</label>
+              <select
+                type="select"
+                className="custom-select m-2"
+                id="state-name"
+                onChange={this.stateChange}
+                >
+                  <option value=''>Select...</option>
+                  {states.map((singleState) => (<option key={singleState.id} value={singleState.id}>{singleState.name}</option>))}
+            </select>
+          </div>
+        </div>
+        <div className="form-inline d-flex justify-content-center">
+          <div className="form-group row">
+            <label htmlFor="county-name" className="col-form-label">County</label>
+            <input
+            type="text"
+            className="form-control m-2"
+            id="county-name"
+            placeholder="Enter County"
+            value={county}
+            onChange={this.countyChange}
             >
-          </input>
+            </input>
+          </div>
         </div>
-        <div className="form-group">
-          <label htmlFor="state-name m-3">Select State</label>
-          <select
-            type="select"
-            className="custom-select col-4 m-2"
-            id="state-name"
-            onChange={this.stateChange}
+        <div className="form-inline d-flex justify-content-center">
+          <div className="form-check">
+            <input type="checkbox"
+            className="form-check-input m-2"
+            id="is-identified"
+            checked={identified}
+            onChange={this.changeIdentified}>
+            </input>
+            <label htmlFor="is-identified">Identified?</label>
+          </div>
+        </div>
+        <div className="form-inline d-flex justify-content-center">
+          <div className="form-group row">
+            <label htmlFor="snake-name" className="col-form-label">Select Snake Species</label>
+            <select
+              type="select"
+              className="custom-select m-2"
+              id="snake-name"
+              onChange={this.snakeChange}
+              disabled={identified ? null : 'disabled'}
+              >
+                <option value=''>Select...</option>
+                {snakes.map((snake) => (<option key={snake.id} value={snake.id}>{snake.commonName}</option>))}
+            </select>
+          </div>
+        </div>
+        <div className="form-inline d-flex justify-content-center">
+          <div className="form-group row">
+            <label htmlFor="snake-image" className="col-form-label">Image Url:</label>
+            <input
+            type="text"
+            className="form-control m-2"
+            id="snake-image"
+            placeholder="Enter Image Url"
+            value={imageUrl}
+            onChange={this.urlChange}
             >
-              {states.map((singleState) => (<option key={singleState.id} value={singleState.id}>{singleState.name}</option>))}
-          </select>
+            </input>
+          </div>
         </div>
-        <div className="form-group">
-          <label htmlFor="county-name">County</label>
-          <input
-          type="text"
-          className="form-control col-4 m-auto"
-          id="county-name"
-          placeholder="Enter County"
-          value={county}
-          onChange={this.countyChange}
-          >
-          </input>
-        </div>
-        <div className="form-group">
-          <label htmlFor="snake-name">Select Snake</label>
-          <select
-            type="select"
-            className="custom-select col-4 m-2"
-            id="snake-name"
-            onChange={this.snakeChange}
+        <div className="form-inline d-flex justify-content-center">
+          <div className="form-group row">
+            <label htmlFor="sighting-description" className="col-form-label">Description of incident:</label>
+            <textarea
+            type="text"
+            className="form-control m-2 col-12"
+            id="sighting-description"
+            placeholder="Enter description of events"
+            value={description}
+            onChange={this.descriptionChange}
+            rows="3"
             >
-              {snakes.map((snake) => (<option key={snake.id} value={snake.id}>{snake.commonName}</option>))}
-          </select>
+            </textarea>
+          </div>
         </div>
+        <button className="btn btn-dark" onClick={this.saveSightingEvent}>Make Report</button>
         </form>
       </div>
     );
