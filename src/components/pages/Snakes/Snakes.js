c36352b29886304cc0ca4ae47c2b1b8fb2ffd1f2
Laura Collins <laura.e.collins1@gmail.com>
2020-01-25 12:00:29 -0600

    I have a search bar

--- src/components/pages/Snakes/Snakes.js
+++ src/components/pages/Snakes/Snakes.js
@@ -31,11 +31,25 @@ class Snakes extends React.Component {
     return (
       <div className="Snakes">
         <h1>Snakes of North America</h1>
+        <div className="container">
+          <form className="row justify-content-center">
+            <div className="form-group">
+              <div className="form-inline">
+                <input
+                type="text"
+                className="form-control m-1"
+                id="search-bar"
+                placeholder="Search...">
+                </input>
+                <label htmlFor="search-bar" className="col-form-label"><button className="btn btn-outline-secondary searchButton"><i className="fas fa-search"></i></button></label>
+              </div>
+            </div>
+          </form>
+        </div>
+        <Link className="btn btn-dark" to="/identify">Identify</Link>
         <div className="snakeContainer container d-flex flex-wrap">
           {this.state.snakes.map((snake) => <Snake key={snake.id} snake={snake} />)}
         </div>
-
-        <Link className="btn btn-dark" to="/identify">Identify</Link>
       </div>
     );
   }
